0a5aaa0d6bfdf9c5752271c8404bc569223c26f2
Yisrael Gurkow <yisrelg3@gmail.com>
2021-06-28 17:15:48 -0400

    commit tree 5697c6188ddeec274f5137b0334265e5aa09b539

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,9 @@
 function reverseString(str) {
-  // type your code here
+  let reversed = ''
+  for (let i = 0; i < str.length; i++) {
+    reversed = str[i] + reversed
+  }
+  return reversed
 }
 
 if (require.main === module) {
@@ -16,4 +20,15 @@ if (require.main === module) {
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
+
+// initialize empty string to hold new word
+// itirate over word starting from last letter
+// add eache letter of itiration to empty string until beginning of string is reached.
+
+
 // And a written explanation of your solution
+
+// I initialized a variable "reversed" to an empty string. 
+// I started the counter from 1 and looked for the index of str.length minus the counter
+// added that letter to the empty string then added 1 to the counter and repeat.
+// when i finished the iterations I returned the reversed word.
--- ruby/reverse_string.rb
+++ ruby/reverse_string.rb
@@ -1,5 +1,13 @@
+require 'pry'
 def reverse_string(str)
-  # type your code in here
+  reversed = ''
+  counter = str.size - 1
+  str.each_char do |char|
+    # binding.pry
+    reversed << str[counter]
+    counter -= 1
+  end
+  reversed
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -15,4 +23,9 @@ if __FILE__ == $PROGRAM_NAME
 end
 
 # Please add your pseudocode to this file
+
+# create counter = 1
+# iterate over the word starting from word.length - counter to start with the last letter
+# counter + 1
+
 # And a written explanation of your solution
